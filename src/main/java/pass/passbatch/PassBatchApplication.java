822049853028d7cc67a6561e4ebc66281f101df2
yun <yshyeon102@gmail.com>
2023-01-15 06:43:03 +0900

    스프링 프로젝트 세팅

---
+++ src/main/java/pass/passbatch/PassBatchApplication.java
@@ -0,0 +1,13 @@
+package pass.passbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassBatchApplication.class, args);
+    }
+
+}
